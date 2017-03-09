b8020360cee9d3ec22e476c7b148eae76c9de864
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-08 23:42:51 -0500

    exposed KeyMutableMap and ValueMutableMap

--- src/main/collection/mutable/index.ts
+++ src/main/collection/mutable/index.ts
@@ -2,3 +2,5 @@ export * from './LinkedSeq';
 export * from './ArraySeq';
 export * from './KeyMutableSeq';
 export * from './ValueMutableSeq';
+export * from './KeyMutableMap';
+export * from './ValueMutableMap';
