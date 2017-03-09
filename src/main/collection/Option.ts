bbb61e19d12f6d7f0dcad92e4878667867c116b8
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-09 12:12:18 -0500

    added Option

--- src/main/collection/Option.ts
+++ src/main/collection/Option.ts
@@ -1,22 +1,31 @@
-// export abstract class Option<T> {
-//   abstract get(): T;
-// }
+export abstract class Option<T> {
+  abstract get(): T;
+}
 
-// export class Some<T> extends Option<T> {
-//   t: T;
+export class Some<T> extends Option<T> {
+  t: T;
 
-//   constructor(t: T) {
-//     super();
-//     this.t = t;
-//   }
+  constructor(t: T) {
+    super();
+    this.t = t;
+  }
 
-//   get(): T {
-//     return this.t;
-//   }
-// }
+  get(): T {
+    return this.t;
+  }
 
-// export class None<T> extends Option<T> {
-//   get(): T {
-//     throw new Error('Cannot call get on None.');
-//   }
-// }
+  static of<T>(t: T): Some<T> {
+    return new Some(t);
+  }
+}
+
+/**
+ * This is perfectly assignable to Option<T> for any T, 
+ * not because TypeScript has covariance suppor, but because
+ * TypesScript doesn't check the variance at all. 
+ */
+export class None extends Option<never> {
+  get(): never {
+    throw new Error('Cannot call get on None.');
+  }
+}
---
+++ src/test/OptionTest.ts
@@ -0,0 +1,4 @@
+import { Option, Some, None } from '../main/collection/Option';
+
+const x: Option<string> = new Some('asdf');
+const y: Option<string> = new None();
