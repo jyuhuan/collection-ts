729699192fb10c25e4327e4f68872318f4bb230f
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-08 23:42:13 -0500

    added testing for the hash table implementation

--- src/test/ClosedAddressHashTableTest.ts
+++ src/test/ClosedAddressHashTableTest.ts
@@ -22,7 +22,7 @@ const stringHash2 = {
   }
 }
 
-const tbl = new ClosedAddressHashTable(stringEq, stringHash2, 5, 1.0);
+const tbl = new ClosedAddressHashTable<string, number>(stringEq, stringHash2, 5, 1.0);
 
 tbl.add('alice', 5);
 tbl.add('bob', 3);
@@ -83,5 +83,26 @@ tbl.remove('lisa');
 tbl.remove('michael');
 tbl.remove('nancy');
 tbl.remove('oliver');
+
+tbl.add('alice', 5);
+tbl.add('bob', 3);
+tbl.add('cathy', 5);
+tbl.add('daniel', 6);
+tbl.add('emilia', 6);
+tbl.add('frank', 5);
+tbl.add('gina', 4);
+tbl.add('howard', 6);
+tbl.add('jacqueline', 10);
+tbl.add('kevin', 5);
+tbl.add('lisa', 4);
+tbl.add('michael', 7);
+tbl.add('nancy', 5);
+tbl.add('oliver', 6);
+
+for (const e of tbl.entires()) {
+  console.log(e.key);
+}
+
+tbl.get('jacqueline')
 
 const bp = 0;
