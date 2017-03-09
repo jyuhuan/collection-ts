ec8104fc108cda10a2eff9aa2dbe5a31fa7732cc
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-09 13:24:47 -0500

    copied code to mimic mlutiple inheritance

--- src/main/collection/mutable/KeyMutableRandomAccessSeq.ts
+++ src/main/collection/mutable/KeyMutableRandomAccessSeq.ts
@@ -1,5 +1,9 @@
 import { ValueMutableRandomAccessSeq } from "./ValueMutableRandomAccessSeq";
 
+// Had to copy code from ValueMutableSeq, because TypeScript doesn't allow multiple inheritance.
 export abstract class KeyMutableRandomAccessSeq<X> extends ValueMutableRandomAccessSeq<X> {
-
+  abstract prepend(x: X): void;
+  abstract append(x: X): void;
+  abstract clear(): void;
+  abstract insert(idx: number, x: X): void;
 }
--- src/main/collection/mutable/ValueMutableRandomAccessSeq.ts
+++ src/main/collection/mutable/ValueMutableRandomAccessSeq.ts
@@ -1,5 +1,16 @@
 import { RandomAccessSeq } from '../RandomAccessSeq';
 
+// Had to copy code from ValueMutableSeq, because TypeScript doesn't allow multiple inheritance.
 export abstract class ValueMutableRandomAccessSeq<X> extends RandomAccessSeq<X> {
+  abstract set(idx: number, x: X): void;
+
+  swap(i: number, j: number): void {
+    const t = this.get(i);
+    this.set(i, this.get(j));
+    this.set(j, t);
+  }
+
+  abstract reverse(): void;
+  abstract reverseSubseq(start: number, end: number): void;
 
 }
