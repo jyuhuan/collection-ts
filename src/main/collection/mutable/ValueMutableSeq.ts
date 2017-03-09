72ca53fe6b6b779c0bec191d89b04485e1131de4
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-09 18:21:17 -0500

    changed swap implementation due to design change in Seq

--- src/main/collection/mutable/ValueMutableSeq.ts
+++ src/main/collection/mutable/ValueMutableSeq.ts
@@ -2,12 +2,32 @@ import { Seq } from '../Seq';
 
 export abstract class ValueMutableSeq<X> extends Seq<X> {
 
-  abstract set(idx: number, x: X): void;
+  abstract changeTo(x: X): void;
+  abstract tail(): ValueMutableSeq<X>;
 
+  set(idx: number, x: X): void {
+    const a: ValueMutableSeq<X> = this.tailAt(idx);
+  }
+
+  // Code is almost the same as in Seq
+  tailAt(idx: number): ValueMutableSeq<X> {
+    if (idx < 0) throw new Error(`Index ${idx} is out of bounds!`);
+    let cur: ValueMutableSeq<X> = this;
+    let i = 0;
+    while (i < idx) {
+      if (cur.tail().isEmpty()) throw new Error(`Index ${idx} is out of bounds!`);
+      i += 1;
+      cur = cur.tail();
+    }
+    return cur;
+  }
+  
   swap(i: number, j: number): void {
-    const t = this.get(i);
-    this.set(i, this.get(j));
-    this.set(j, t);
+    const nodeI = this.tailAt(i);
+    const nodeJ = this.tailAt(j);
+    const t = nodeI.head();
+    nodeI.changeTo(nodeJ.head());
+    nodeJ.changeTo(t);
   }
 
   abstract reverse(): void;
