e5e764492a909b9d06c09b7fb836c5c6ff9ef474
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-10 10:26:47 -0500

    removed unused tests

--- src/test/OptionTest.ts
+++
@@ -1,4 +0,0 @@
-import { Option, Some, None } from '../main/collection/Option';
-
-const x: Option<string> = new Some('asdf');
-const y: Option<string> = new None();
--- src/test/RangeTest.ts
+++
@@ -1,9 +0,0 @@
-import * as tc from '../main/collection';
-
-const r = new tc.Range(3, 8);
-const rr = r.reversed();
-
-console.log(r.toString());
-console.log(rr.toString());
-
-const bp = 0;
