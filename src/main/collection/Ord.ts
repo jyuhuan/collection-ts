83f1951c870582568e1b083d16b9e53a8882a8de
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-09 23:12:35 -0500

    added default Ord operations

--- src/main/collection/Ord.ts
+++ src/main/collection/Ord.ts
@@ -8,6 +8,22 @@
  * @version 0.0.0
  * @since 0.0.0
  */
-export interface Ord<X> {
-  compare(x1: X, x2: X): number
+export abstract class Ord<X> {
+  abstract compare(x1: X, x2: X): number;
+
+  le(a: X, b: X): boolean { return this.compare(a, b) <= 0; }
+  ge(a: X, b: X): boolean { return this.compare(a, b) >= 0; }
+  lt(a: X, b: X): boolean { return this.compare(a, b) < 0; }
+  gt(a: X, b: X): boolean { return this.compare(a, b) > 0; }
+  eq(a: X, b: X): boolean { return this.compare(a, b) === 0; }
+  ne(a: X, b: X): boolean { return this.compare(a, b) !== 0; }
+
+  max(a: X, b: X): X {
+    return this.ge(a, b) ? a : b;
+  }
+
+  min(a: X, b: X): X {
+    return this.lt(a, b) ? a : b;
+  }
+
 }
