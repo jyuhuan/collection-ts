38a88f478c69fe0645cf1aabc69b5d83192271bc
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-09 23:13:45 -0500

    added construction methods for HashSet

--- src/main/collection/mutable/HashSet.ts
+++ src/main/collection/mutable/HashSet.ts
@@ -1,8 +1,9 @@
 import { Set } from '../Set';
-import { Eq } from '../Eq';
+import { Eq, DefaultJavaScriptEq } from '../Eq';
 import { Iterable } from '../Iterable';
 import { ClosedAddressHashTable } from '../base/ClosedAddressHashTable';
 import { KeyMutableSet } from './KeyMutableSet';
+import { Hash, DefaultJavaScriptHash } from '../Hash';
 
 export class HashSet<K> extends KeyMutableSet<K> {
   tbl: ClosedAddressHashTable<K, any>;
@@ -35,5 +36,17 @@ export class HashSet<K> extends KeyMutableSet<K> {
   clear(): void {
     this.tbl.clear();
   }
+
+
+  static from<X>(...elems: X[]): HashSet<X> {
+    return HashSet.fromUnder<X>(new DefaultJavaScriptEq<X>(), new DefaultJavaScriptHash<X>(), ...elems);
+  }
+
+  static fromUnder<X>(eq: Eq<X>, hash: Hash<X>, ...elems: X[]): HashSet<X> {
+    const tbl = ClosedAddressHashTable.emptyUnder<X, any>(eq, hash);
+    for (const elem of elems) tbl.add(elem, null);
+    return new HashSet(tbl);
+  }
+
 
 }
